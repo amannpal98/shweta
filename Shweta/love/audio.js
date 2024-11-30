ddf920189779dce81ec35e525fbe137914e62127
Aman Pal <143773710+amannpal98@users.noreply.github.com>
2024-11-30 23:27:12 +0530

    Delete Shweta directory

--- Shweta/love/audio.js
+++
@@ -1,8 +0,0 @@
-// Wait until the page is fully loaded
-window.addEventListener('load', () => {
-  const audio = document.getElementById('myAudio');
-  audio.muted = false; // Remove muted if not needed
-  audio.play().catch(error => {
-    console.error('Autoplay blocked:', error);
-  });
-});
